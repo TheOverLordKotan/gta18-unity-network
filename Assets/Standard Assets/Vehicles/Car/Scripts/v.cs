905ad6dc257b083bb3a072dea9666303092ffadb
TheOverLordKotan <ada@ue.edu.co>
2019-06-25 15:25:01 -0500

    Integration and release 0

---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/v.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+namespace UnityStandardAssets.Vehicles.Car
+{
+public class v : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<CarController>().enabled = true;
+			GetComponent<CarUserControl>().enabled = true;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
